e8a911338a6d0c1369fc590ab227093ee536b5a7
magicHu <huronghai2008@gmail.com>
2012-10-30 23:15:33 +0800

    madhouse

--- app/controllers/madhouse_controller.rb
+++ app/controllers/madhouse_controller.rb
@@ -7,11 +7,24 @@ class MadhouseController < ApplicationController
   	@url = "http://www.bshare.local/bshare_website/" + "api/user/profile"
 
   	@params = {}
+  	@params["publisherUuid"] = "1234-5678"
+  	@params["ts"] = Time.now
+
+  	@secret = "9999"
+  	@params["sig"] = sign(@params, @secret)
   	
+  end
+
+  def sign(params, secret)
+  	""
   end
 
 
   def generate_url(base_url, params)
+
+  end
+
+  def toStr(params)
 
   end
 
