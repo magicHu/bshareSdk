f7b4b2ac4d957173c73522439a3e1b0dbfc56ec4
lingdongrushui <lingdongrushui@gmail.com>
2013-09-18 14:18:44 +0800

    improve perfermance.

--- lib/bshareone/extract_data.rb
+++ lib/bshareone/extract_data.rb
@@ -4,27 +4,27 @@ require 'roo'
 
 OUT_PUT_FILE = "migo.txt"
 
-def extract_data_from_excel(file_path)
+def extract_data_from_excel(file_path, out_put_file)
   excel = Roo::Excel.new(file_path)
   excel.default_sheet = excel.sheets.first
 
-  File.open(OUT_PUT_FILE, "a") do |file|
-    2.upto(excel.last_row) do |line|
-      content = excel.cell(line, 'B')
-      time    = excel.cell(line, 'C')
-      uid     = excel.cell(line, 'D')
-      type    = 0
-      if ('True' == excel.cell(line, 'H')) 
-        type = 1 
-      end
+  2.upto(excel.last_row) do |line|
+    content = excel.cell(line, 'B')
+    time    = excel.cell(line, 'C')
+    uid     = excel.cell(line, 'D')
+    type    = 0
+    if ('True' == excel.cell(line, 'H')) 
+      type = 1 
+    end
 
-      if content
-        file.puts "{\"u\":\"#{uid}\",\"tweet\":\"#{content}\",\"type\":\"#{type}\",\"t\":\"#{time}\"}"
-      end
+    if content
+      out_put_file.puts "{\"u\":\"#{uid}\",\"tweet\":\"#{content}\",\"type\":\"#{type}\",\"t\":\"#{time}\"}"
     end
   end
 end
 
-Dir["data/*微博列表*.xls"].each do |file_name|
-  extract_data_from_excel(file_name)
+File.open(OUT_PUT_FILE, "a") do |file|
+  Dir["data/**/*微博列表*.xls"].each do |file_name|
+    extract_data_from_excel(file_name, file)
+  end
 end
